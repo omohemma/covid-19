c7efbdc47a3addf1ebb270cecd291ff77e95804c
omohemma <emmaomoh@gmail.com>
2020-04-13 08:33:52 +0100

    Setup PurgeCss

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  plugins: [require("tailwindcss"), require("autoprefixer")]
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    process.env.NODE_ENV === "production" &&
+      require("@fullhuman/postcss-purgecss")({
+        // To specify the files to look for
+        content: ["./src/*/*.vue", "./public/index.html"],
+        // Regex to look out for classname in files
+        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+      })
+  ]
 };
